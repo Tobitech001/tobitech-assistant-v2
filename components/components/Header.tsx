b584ffea3c55ef07c354f32b662dce90694fc164
Tobitech001 <moshoodabdulmujibtobiloba@gmail.com>
2025-07-27 19:02:19 +0100

    Update Header.tsx

--- components/components/Header.tsx
+++ components/components/Header.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 export default function Header() {
   return (
-    <header className="bg-black text-white p-4 shadow">
-      <h1 className="text-xl font-bold">Tobii Tech Assistant</h1>
+    <header className="bg-gray-800 text-white px-4 py-3 shadow-md">
+      <h1 className="text-xl font-bold">🤖 TOBI TECH Assistant</h1>
     </header>
   );
 }
